ac338f74a1250ad46d2ac36199cbf1a5e6ad65da
Matt <carlotta.matt@gmail.com>
2019-03-05 16:11:42 -0800

    Update env file

--- envs/index.js
+++ envs/index.js
@@ -8,7 +8,7 @@ module.exports = {
   currentDirectory, // current working directory
   inDevelopment: env === "development", // working in development ENV
   hashedMediaName: "[name].[hash:8].[ext]", // hased name gives to images or fonts
-  HOST: process.env.NODE_HOST || "localhost", // current host
+  HOST: process.env.HOST || "localhost", // current host
   localIdentName: "[folder]_[name]__[local]--[hash:base64:5]", // how the CSS class names will be declared in the DOM
   nodeENV: env || "development", // current envirnoment
   PORT: process.env.PORT || 3000, // current express port
