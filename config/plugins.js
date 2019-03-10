8588290862d4a14baaecc501ea97db9c4c86a442
Matt <carlotta.matt@gmail.com>
2019-03-09 20:37:21 -0800

    Fix compilation message

--- config/plugins.js
+++ config/plugins.js
@@ -64,7 +64,7 @@ export default () => {
           ],
           notes: [
             "Note that the development build is not optimized.",
-            "To create a production build, use \x1b[1m\x1b[32mnpm run build\x1b[0m.\n"
+            "To create a production build, use \x1b[1m\x1b[32myarn build\x1b[0m.\n"
           ]
         },
         clearConsole: true
