ae9d3c1ee80be44b91bf3b5da64cf9203ad0b4ed
Matt <carlotta.matt@gmail.com>
2019-03-20 16:13:08 -0700

    Fix App test

--- src/components/App/__tests__/App.test.js
+++ src/components/App/__tests__/App.test.js
@@ -1,16 +1,22 @@
 import { createBrowserHistory } from "history";
+import Home from "pages/Home/Home";
+import configureStore from "store/configureStore";
 import { App } from "../App";
-import routes from "../../../routes";
-import configureStore from "../../../store/configureStore";
 
 const history = createBrowserHistory();
 const initialState = window.__INITIAL_STATE__;
 const store = configureStore(history, initialState);
 
+const route = {
+  path: "/",
+  exact: true,
+  component: Home
+};
+
 const wrapper = mount(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App route={routes} />
+      <App route={route} />
     </ConnectedRouter>
   </Provider>
 );
